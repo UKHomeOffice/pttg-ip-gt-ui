a6d132ca86ae7478103eb792d59f25bb01c4cfb8
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-10-07 12:31:18 +0100

    reworked docker build and scripts

--- ci_build.sh
+++ ci_build.sh
@@ -17,7 +17,8 @@ build_app() {
 
   docker rm -v $(docker ps -a -q -f status=exited)
   docker run --name pttg-ip-gt-ui-build -e ${ENV_OPTS}  "${GRADLE_IMAGE}" "${@}"
-  sudo docker cp pttg-ip-gt-ui-build:/work/build/libs/pttg-ip-gt-ui-${VERSION}.${GIT_COMMIT}.jar build/libs
+  mkdir -p build/libs
+  docker cp pttg-ip-gt-ui-build:/work/build/libs/pttg-ip-gt-ui-${VERSION}.${GIT_COMMIT}.jar build/libs
 }
 
 set_props() {
