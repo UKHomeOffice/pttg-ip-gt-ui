d1de5ef24762dce4b3228c7ff85f778fede84de6
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-10-07 12:23:23 +0100

    reworked docker build and scripts

--- ci_build.sh
+++ ci_build.sh
@@ -15,7 +15,7 @@ build_app() {
   ENV_OPTS="GIT_COMMIT=${GIT_COMMIT} -e VERSION=${VERSION}"
   [ -n "${BUILD_NUMBER}" ] && ENV_OPTS="BUILD_NUMBER=${BUILD_NUMBER} -e ${ENV_OPTS}"
 
-  #docker rm -v $(docker ps -a -q -f status=exited)
+  docker rm -v $(docker ps -a -q -f status=exited)
   docker run --name pttg-ip-gt-ui-build -e ${ENV_OPTS}  "${GRADLE_IMAGE}" "${@}"
   sudo docker cp pttg-ip-gt-ui-build:/work/build/libs/pttg-ip-gt-ui-${VERSION}.${GIT_COMMIT}.jar build/libs
 }
