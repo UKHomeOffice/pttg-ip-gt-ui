df1d02b398a58c810df5ae804063aa3c4c7b6d83
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-10-18 13:08:12 +0100

    Fix feature file directory name

--- src/test/java/acceptance/AcceptanceTests.java
+++ src/test/java/acceptance/AcceptanceTests.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features={"src/test/specs/income-proving/generic-tool/design-v1"}, glue={"steps"}/*,  tags = {"@SD63"}*/)
+@CucumberOptions(features={"src/test/specs/income-proving/generic-tool"}, glue={"steps"}/*,  tags = {"@SD63"}*/)
 public class AcceptanceTests {}
